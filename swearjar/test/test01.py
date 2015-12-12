2c32b4e9a159a34d5aa1865127e039eeac200779
Paul Boal <paul.boal@gmail.com>
2015-12-11 20:42:47 -0600

    converted test01 to a unittest

--- swearjar/test/test01.py
+++ swearjar/test/test01.py
@@ -2,16 +2,24 @@
 import sys
 import logging
 sys.path.append ("../../")
-
 import swearjar
 
+import unittest
 
-t = swearjar.SwearJar(cmd="./curses01.py")
+class TestSwearJar(unittest.TestCase):
+
+    def test01(self):
+        t = swearjar.SwearJar(cmd="./curses01.py")
 
 
-# Test #1 See if we can find a menu that looks like "1. Edit Data"
-(r,c) = t.expect("1. Edit", timeout=1)
-# (r,c) = t.expect_locate("Menu", "1. Edit", timeout=1)
+        # Test #1 See if we can find a menu that looks like "1. Edit Data"
+        (r,c) = t.expect("1. Edit", timeout=1)
+        # (r,c) = t.expect_locate("Menu", "1. Edit", timeout=1)
 
-print (t.ascii())
-print ("We found our string at %d, %d"%(r,c))
+        # print (t.ascii())
+        # print ("We found our string at %d, %d"%(r,c))
+        self.assertEqual(r,5)
+        self.assertEqual(c,4)
+
+if __name__ == '__main__':
+    unittest.main()
